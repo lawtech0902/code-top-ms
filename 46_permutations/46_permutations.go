3d96cfd7d9ccbb3c5c0b64cfba76a5759b9d112a
lawtech <584563542@qq.com>
2022-11-09 17:59:48 +0800

    2022 11 09

---
+++ 46_permutations/46_permutations.go
@@ -0,0 +1,28 @@
+package permutations
+
+func permute(nums []int) [][]int {
+	var (
+		track []int
+		res   [][]int
+	)
+
+	backTracking(nums, track, &res)
+	return res
+}
+
+func backTracking(nums, track []int, res *[][]int) {
+	if len(nums) == 0 {
+		temp := make([]int, len(track))
+		copy(temp, track)
+		*res = append(*res, temp)
+	}
+
+	for i := 0; i < len(nums); i++ {
+		cur := nums[i]
+		track = append(track, cur)
+		nums = append(nums[:i], nums[i+1:]...)
+		backTracking(nums, track, res)
+		nums = append(nums[:i], append([]int{cur}, nums[i:]...)...)
+		track = track[:len(track)-1]
+	}
+}
