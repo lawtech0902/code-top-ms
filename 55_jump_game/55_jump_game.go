a813c622bada38c9fcc246a46d67d3e175ccd89b
lawtech <584563542@qq.com>
2022-11-14 09:41:26 +0800

    2022 11 13

---
+++ 55_jump_game/55_jump_game.go
@@ -0,0 +1,25 @@
+package jumpgame
+
+func canJump(nums []int) bool {
+	if len(nums) == 1 {
+		return true
+	}
+
+	cover := 0
+	for i := 0; i <= cover; i++ {
+		cover = max(cover, i+nums[i])
+		if cover > len(nums)-1 {
+			return true
+		}
+	}
+
+	return false
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
